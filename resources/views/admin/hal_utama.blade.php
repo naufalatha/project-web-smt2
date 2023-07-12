6005c71937ec19746a2d384b9cbff4bcee1b780d
FrostMiner11 <yudiwaliyuddin3@gmail.com>
2023-07-12 19:06:31 +0700

    menambahkan_admin

--- resources/views/admin/hal_utama.blade.php
+++ resources/views/admin/hal_utama.blade.php
@@ -6,24 +6,24 @@
             <div class="col-md-4">
                 <div
                     style="background-color: #dfdfdf; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-                    <h4>PEMINJAMAN BARU</h4>
-                    <h4 style="font-size: 56pt;"><b>// jumlah Peminnjam baru</b></h4>
+                    <h4 class="text-center">PEMINJAMAN BARU</h4>
+                    <h4 style="font-size: 56pt;"class="text-center"><b></b></h4>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div
                     style="background-color: #dfdfdf; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-                    <h4>PEMINJAMAN DIBATALKAN</h4>
-                    <h4 style="font-size: 56pt;"><b>// jumlah peminjam dibatalkan</b></h4>
+                    <h4 class="text-center">PEMINJAMAN DIBATALKAN</h4>
+                    <h4 style="font-size: 56pt;" class="text-center"><b></b></h4>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div
                     style="background-color: #dfdfdf; padding-bottom: 60px; padding-left: 20px;padding-right: 20px; padding-top: 10px;">
-                    <h4>PEMINJAMAN DITERIMA</h4>
-                    <h4 style="font-size: 56pt;"><b>// jumlah peminjam diterima</b></h4>
+                    <h4 class="text-center">PEMINJAMAN DITERIMA</h4>
+                    <h4 style="font-size: 56pt;" class="text-center"><b></b></h4>
                 </div>
             </div>
         </div>
